0653c08e825d8dec8d40b8c24999b9cd4e051cc4
strdnrm <93453304+strdnrm@users.noreply.github.com>
2022-03-04 00:43:53 +0300

    added some queries for task 2

--- 2_1task.sql
+++ 2_1task.sql
@@ -17,3 +17,6 @@ SELECT n_group, AVG(score) FROM student GROUP BY n_group HAVING AVG(score) > 3.5
 --8
 SELECT n_group, COUNT(n_group), MAX(score), AVG(score), MIN(score) FROM student GROUP BY n_group;
 --9
+SELECT * FROM student WHERE score IN (SELECT MAX(score) FROM student WHERE n_group = 2231 GROUP BY n_group);
+--10
+SELECT * FROM student WHERE (n_group, score) IN (SELECT n_group, MAX(score) FROM student GROUP BY n_group);
