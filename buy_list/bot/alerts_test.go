ef2f71d22d876683ba7aa21d0556f1e66ce25b15
strdnrm <93453304+strdnrm@users.noreply.github.com>
2022-11-18 16:42:54 +0300

    Added test for alerts

--- buy_list/bot/alerts_test.go
+++ buy_list/bot/alerts_test.go
@@ -5,11 +5,12 @@ import (
 	"context"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/go-co-op/gocron"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"go.uber.org/zap"
 )
-
-var token string = os.Getenv("tgtoken")
 
 func TestInitScheduler(t *testing.T) {
 	b, err := tgbotapi.NewBotAPI(os.Getenv("tgtoken"))
@@ -20,10 +21,52 @@ func TestInitScheduler(t *testing.T) {
 		BotAPI: b,
 		s: &StorerMock{
 			GetTodayBuyListFunc: func(ctx context.Context) ([]models.Product, error) {
-				return []models.Product{}, nil
+				return []models.Product{
+					{
+						UserId:    "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:     0,
+						Name:      "Pillow",
+						Weight:    10.0,
+						BuyDate:   "2006-01-02T15:04:05Z",
+					},
+					{
+						UserId:    "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:     0,
+						Name:      "Mango",
+						Weight:    10.0,
+						BuyDate:   "2006-01-02T15:04:05Z",
+					},
+				}, nil
+			},
+			GetChatIdByUserIdFunc: func(ctx context.Context, userid string) (int64, error) {
+				return 1019642784, nil
+			},
+			GetSoonExpireListFunc: func(ctx context.Context) ([]models.FridgeProduct, error) {
+				return []models.FridgeProduct{
+					{
+						UserId:      "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId:   "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:       0,
+						Name:        "Pillow",
+						Opened:      false,
+						Expire_date: "2006-01-02T15:04:05Z",
+					},
+					{
+						UserId:      "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId:   "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:       0,
+						Name:        "Mango",
+						Opened:      false,
+						Expire_date: "2006-01-02T15:04:05Z",
+					},
+				}, nil
 			},
 		},
 	}
+	logger = zap.NewExample()
+	defer logger.Sync()
 	InitScheduler(&bot)
 	if !schb.IsRunning() {
 		t.Error("Buy list alerts not running")
@@ -35,10 +78,89 @@ func TestInitScheduler(t *testing.T) {
 }
 
 func TestCreateBuyAlerts(t *testing.T) {
+	b, err := tgbotapi.NewBotAPI(os.Getenv("tgtoken"))
+	if err != nil {
+		t.Error(err)
+	}
+	bot := Bot{
+		BotAPI: b,
+		s: &StorerMock{
+			GetTodayBuyListFunc: func(ctx context.Context) ([]models.Product, error) {
+				return []models.Product{
+					{
+						UserId:    "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:     0,
+						Name:      "Pillow",
+						Weight:    10.0,
+						BuyDate:   time.Now().Add(time.Hour).Format(time.RFC3339),
+					},
+					{
+						UserId:    "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:     0,
+						Name:      "Mango",
+						Weight:    10.0,
+						BuyDate:   time.Now().Add(2 * time.Hour).Format(time.RFC3339),
+					},
+				}, nil
+			},
+			GetChatIdByUserIdFunc: func(ctx context.Context, userid string) (int64, error) {
+				return 1019642784, nil
+			},
+		},
+	}
+	logger = zap.NewExample()
+	defer logger.Sync()
+	schb = gocron.NewScheduler(time.Local)
+	CreateBuyAlerts(&bot)
+	if len(schb.Jobs()) != 2 {
+		t.Errorf("Want %d jobs got %d", 2, len(schb.Jobs()))
+	}
 
 }
 
 func TestCreateExpireAlerts(t *testing.T) {
+	b, err := tgbotapi.NewBotAPI(os.Getenv("tgtoken"))
+	if err != nil {
+		t.Error(err)
+	}
+	bot := Bot{
+		BotAPI: b,
+		s: &StorerMock{
+			GetSoonExpireListFunc: func(ctx context.Context) ([]models.FridgeProduct, error) {
+				return []models.FridgeProduct{
+					{
+						UserId:      "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId:   "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:       0,
+						Name:        "Pillow",
+						Opened:      false,
+						Expire_date: time.Now().Add(time.Hour).Format(time.RFC3339),
+					},
+					{
+						UserId:      "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						ProductId:   "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+						State:       0,
+						Name:        "Mango",
+						Opened:      false,
+						Expire_date: time.Now().Add(time.Hour).Format(time.RFC3339),
+					},
+				}, nil
+			},
+			GetChatIdByUserIdFunc: func(ctx context.Context, userid string) (int64, error) {
+				return 1019642784, nil
+			},
+		},
+	}
+	logger = zap.NewExample()
+	defer logger.Sync()
+	schf = gocron.NewScheduler(time.Local)
+	CreateExpireAlerts(&bot)
+	// zero because we send notifications immediately
+	if len(schf.Jobs()) != 0 {
+		t.Errorf("Want %d jobs got %d", 0, len(schf.Jobs()))
+	}
 
 }
 
